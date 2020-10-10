f444eed9803b29de0dab9ad6c6a5a6fd90107d91
Michael Ida <drmikeida@drmikeida.com>
2020-10-09 14:33:44 -1000

    started working on problem 145

--- project_euler/problem_145/sol1.py
+++ project_euler/problem_145/sol1.py
@@ -19,22 +19,9 @@ def number_of_reversible_given_length(number_of_digits: int) -> int:
                 test_number_string = str(first_digit) + middle_string + str(last_digit)
                 test_number = int(test_number_string)
                 if is_reversible(test_number):
-                    print(test_number)
-                    number_of_reversible += 1
-    for first_digit in range(2, 10, 2):
-        for last_digit in range(1, 10, 2):
-            max_middle_string = (number_of_digits - 2) * '9'
-            for middle in range(0, int(max_middle_string) + 1):
-                middle_string = str(middle)
-                if len(middle_string) < (number_of_digits - 2):
-                    middle_string = (number_of_digits - 2 - len(middle_string)) * '0' + middle_string
-                test_number_string = str(first_digit) + middle_string + str(last_digit)
-                test_number = int(test_number_string)
-                if is_reversible(test_number):
-                    print(test_number)
-                    number_of_reversible += 1
+                    number_of_reversible += 2
     return number_of_reversible
 
 
 if __name__ == '__main__':
-    print(number_of_reversible_given_length(7))
+    print(number_of_reversible_given_length(8))
